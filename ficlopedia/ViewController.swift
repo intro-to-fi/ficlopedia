1635d6a40256dff8b14798dfefc9c6d21eeb42aa
Intro To FI <git@introtofi.com>
2019-05-16 22:19:44 -0700

    Fix whitespace and remove initial comments

--- ficlopedia/ViewController.swift
+++ ficlopedia/ViewController.swift
@@ -6,12 +6,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
-
