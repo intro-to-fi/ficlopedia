bcdd75d6c5da2a2723b0291a9d92fe4b55d5c6c5
Intro To FI <git@introtofi.com>
2019-05-19 00:14:58 -0700

    Layout, and content inset for keyboard

--- ficlopedia/EntryDetailViewController.swift
+++ ficlopedia/EntryDetailViewController.swift
@@ -6,6 +6,7 @@
 import UIKit
 
 class EntryDetailViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var valueTextField: UITextField!
     @IBOutlet weak var descriptionLabel: UILabel!
@@ -19,9 +20,25 @@ class EntryDetailViewController: UIViewController {
         valueTextField.text = entry?.value
         descriptionLabel.text = "Description"
         descriptionTextView.text = entry?.description
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func configure(with entry: Entry) {
         self.entry = entry
+    }
+    
+    @objc
+    func keyboardDidShow(notification: NSNotification) {
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        let keyboardEnd = value.cgRectValue
+        scrollView.contentInset = .init(top: 0, left: 0, bottom: keyboardEnd.height, right: 0)
+    }
+    @objc
+    func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
