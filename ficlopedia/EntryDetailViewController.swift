1016b5abc42763d08945017da851af9e36fff2d5
Intro To FI <git@introtofi.com>
2019-05-19 00:31:48 -0700

    Wire up delegate to detect text changes

--- ficlopedia/EntryDetailViewController.swift
+++ ficlopedia/EntryDetailViewController.swift
@@ -13,13 +13,37 @@ class EntryDetailViewController: UIViewController {
     @IBOutlet weak var descriptionTextView: UITextView!
     
     var entry: Entry?
+    var hasEdits: Bool = false {
+        didSet {
+            if hasEdits {
+                navigationItem.leftBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+                navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .save, target: self, action: #selector(didTapSave))
+            } else {
+                navigationItem.leftBarButtonItem = nil
+                navigationItem.rightBarButtonItem = nil
+            }
+        }
+    }
+    
+    @objc
+    func didTapCancel() {
+        if let entry = entry {
+            load(from: entry)
+        }
+        hasEdits = false
+    }
+    
+    @objc
+    func didTapSave() {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        valueLabel.text = "Value"
-        valueTextField.text = entry?.value
-        descriptionLabel.text = "Description"
-        descriptionTextView.text = entry?.description
+        if let entry = entry {
+            load(from: entry)
+        }
+        valueTextField.delegate = self
+        descriptionTextView.delegate = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -27,6 +51,13 @@ class EntryDetailViewController: UIViewController {
     
     func configure(with entry: Entry) {
         self.entry = entry
+    }
+    
+    func load(from entry: Entry) {
+        valueLabel.text = "Value"
+        valueTextField.text = entry.value
+        descriptionLabel.text = "Description"
+        descriptionTextView.text = entry.description
     }
     
     @objc
@@ -40,5 +71,19 @@ class EntryDetailViewController: UIViewController {
     @objc
     func keyboardWillHide(notification: NSNotification) {
         scrollView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
+extension EntryDetailViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        hasEdits = true
+        return true
+    }
+}
+
+extension EntryDetailViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        hasEdits = true
+        return true
     }
 }
