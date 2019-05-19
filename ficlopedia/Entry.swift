a0309d345c6787c3dc32d8faf3c43f754a74284a
Intro To FI <git@introtofi.com>
2019-05-18 23:11:44 -0700

    Add Entry data model

---
+++ ficlopedia/Entry.swift
@@ -0,0 +1,11 @@
+//
+// Created 5/18/19
+// Copyright © 2019 Intro To FI. All rights reserved.
+//
+
+import Foundation
+
+struct Entry: Codable {
+    let value: String
+    let description: String
+}
